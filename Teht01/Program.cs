e11bda4c929e8602d55406705275518cea423a44
Juuso <k3236@student.jamk.fi>
2017-02-22 19:58:35 +0200

    fdsa

--- Teht01/Program.cs
+++ Teht01/Program.cs
@@ -10,6 +10,20 @@ namespace Teht01
     {
         static void Main(string[] args)
         {
+            string data = "/n";
+            System.IO.StreamWriter outputFile = new System.IO.StreamWriter(@"E:\koulu\Olio-ohjelmointi\test.txt");
+            while (data != "")
+            {
+                Console.WriteLine("Give a text line: ");
+                data = Console.ReadLine();
+                if (data != "")
+                {
+                    outputFile.WriteLine(data);
+                }
+            }
+            outputFile.Close();
+            string text = System.IO.File.ReadAllText(@"E:\koulu\Olio-ohjelmointi\test.txt");
+            System.Console.WriteLine(text);
         }
     }
 }
